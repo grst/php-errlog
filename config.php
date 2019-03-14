7ea5b6b9e14ca02ba1500918788630c63b73263b
Gregor Sturm <mail@gregor-sturm.de>
2019-03-14 17:48:24 +0100

    update config

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 //$GLOBALS['config']['path'] = '../dlhelper2/logs';
 $GLOBALS['config']['path'] = '/home/sturm/repos/web/klickv2/logs';
-//$GLOBALS['config']['path'] = 'D:\programming\htdocs\scms\scms_v2_1\logs';
+//$GLOBALS['config']['path'] = '/home/sturm/repos/web/gymnasium-immenstadt/current/logs';
 $GLOBALS['config']['ext'] = 'txt';
